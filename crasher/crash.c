cae13b88b2b2f9d9434de423427556fbba4e78a3
Charles Chai <charlesxiaoanchai@gmail.com>
2026-09-09 19:28:43 -0400

    Initial commit

---
+++ crasher/crash.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#define CHUNK 1048576
+// Fork amount is 2^FORK_POWER
+#define FORK_POWER 8
+
+void shush_signals(void) {
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    // Loop through all possible signal numbers
+    for (int i = 1; i < NSIG; i++) {
+        sigaction(i, &sa, NULL);
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+	// Crashes a computer by eating its RAM
+	if (argc > 2 || (argc == 2 && strcmp(argv[1], "-f") != 0)) {
+		fprintf(stderr, "Usage: %s [-f]\n", argv[0]);
+		return 1;
+	}
+	char ans[5];
+	if (argc == 1) {
+		// Warn the user
+		printf("WARNING: Running this program will probably eat all of your computer's RAM and crash it. Are you sure you want to continue? (add flag -f to disable warning)\n[y/N]: ");
+		scanf("%4s", ans);
+		if (ans[0] != 'y') {
+			printf("\nHalting...\n");
+			return 0;
+		}
+	}
+	printf("Ok.\n");
+    shush_signals();
+    for (int i = 0; i < FORK_POWER; i++) {
+        fork();
+    }
+    while (1) {
+		char *robber = malloc(CHUNK);
+		memset(robber, 13, sizeof(*robber));
+	}
+	return 0;
+}
