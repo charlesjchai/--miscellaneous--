5ab11f06cb4936946ad03b898fa16c28395fff7d
Charles Chai <charlesxiaoanchai@gmail.com>
2026-09-21 22:08:06 -0400

    DEFINITELY finished two_sum

--- the-worst-two-sum-to-ever-be-invented/two_sum.c
+++ the-worst-two-sum-to-ever-be-invented/two_sum.c
@@ -1,4 +1,4 @@
-/* This is a horrible solution (WIP) for Leetcode's problem "Two sum".
+/* This is a horrible solution for Leetcode's problem "Two sum".
  i love GNU Linux
  */
 
@@ -7,31 +7,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define WHEN_YOU_GAZE_INTO_THE_ABYSS(x) (sizeof(x))
-#define THE_ABYSS_GAZES_BACK int
+static inline size_t sizeof_int() {
+    // Procedural programming!
+    return sizeof(int);
+}
 struct NodeObjectThatIsExtremelyVerboseAndCIsTheBestLanguageBecauseItIsGNU_Linux_andIt_isSO_COOL{
-    uintptr_t KeyThatIsSoCool;
-    uintptr_t ValueThatIsSoCool;
+    uintptr_t KeyThatIsSoC0ol;
+    uintptr_t ValueThatIsSoCo0l;
 };
-int*twoSum(int*nums,int numsSize,int target,int*returnSize){
-    int*indices=calloc(2,WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
-    struct NodeObjectThatIsExtremelyVerboseAndCIsTheBestLanguageBecauseItIsGNU_Linux_andIt_isSO_COOL*MapThatIsSoCool=calloc(numsSize,sizeof(struct NodeObjectThatIsExtremelyVerboseAndCIsTheBestLanguageBecauseItIsGNU_Linux_andIt_isSO_COOL));
-    int*ReallyCo0lMapIndex=malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
-    for(*ReallyCo0lMapIndex=0;*ReallyCo0lMapIndex<numsSize;(*ReallyCo0lMapIndex)++){
-        MapThatIsSoCool[*ReallyCo0lMapIndex].KeyThatIsSoCool=(uintptr_t)malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
-        MapThatIsSoCool[*ReallyCo0lMapIndex].ValueThatIsSoCool=(uintptr_t)malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
+int*twoSum(int*Strange_numBerArrayThing,int Strange_numBerArrayThing_length,int WeirdTargetSumThing,int*ZeroIfItFailedTwoIfItSucceeded){
+    size_t (*WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___)() = sizeof_int;
+    int*indices=calloc(2,WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___());
+    struct NodeObjectThatIsExtremelyVerboseAndCIsTheBestLanguageBecauseItIsGNU_Linux_andIt_isSO_COOL*MapThatIsSoCool=calloc(Strange_numBerArrayThing_length,WHEN_YOU_GAZE_INTO_THE_ABYSS(struct NodeObjectThatIsExtremelyVerboseAndCIsTheBestLanguageBecauseItIsGNU_Linux_andIt_isSO_COOL));
+    int*ReallyCo0lMapIndex=malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___());
+    for(*(int*)ReallyCo0lMapIndex=0;*(int*)ReallyCo0lMapIndex<Strange_numBerArrayThing_length;(*(int*)ReallyCo0lMapIndex)++){ // In strong typing we trust
+        MapThatIsSoCool[*(int*)ReallyCo0lMapIndex].KeyThatIsSoC0ol=(uintptr_t)malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___());
+        MapThatIsSoCool[*(int*)ReallyCo0lMapIndex].ValueThatIsSoCo0l=(uintptr_t)malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___());
     }
     free(ReallyCo0lMapIndex);
     int ReallyCoolMapIndex=0;
-    for(int CloudMCCoolioIndex=0;CloudMCCoolioIndex<numsSize;CloudMCCoolioIndex++){
-        int diff=target-nums[CloudMCCoolioIndex];
+    for(int CloudMCCoolioIndex=0;CloudMCCoolioIndex<Strange_numBerArrayThing_length;CloudMCCoolioIndex++){
+        int diff=WeirdTargetSumThing-Strange_numBerArrayThing[CloudMCCoolioIndex];
         for(int ReallyC0olMapIndex=0;ReallyC0olMapIndex<ReallyCoolMapIndex;ReallyC0olMapIndex++){
-            if(*(int*)(MapThatIsSoCool[ReallyC0olMapIndex].KeyThatIsSoCool)==diff){
-                indices[0]=CloudMCCoolioIndex;indices[1]=*(int*)(MapThatIsSoCool[ReallyC0olMapIndex].ValueThatIsSoCool);
-                *returnSize=2;
-                ReallyCo0lMapIndex=malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
-                for(*ReallyCo0lMapIndex=0;*ReallyCo0lMapIndex<numsSize;(*ReallyCo0lMapIndex)++){
-                    free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].KeyThatIsSoCool);
-                    free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].ValueThatIsSoCool);
+            if(*(int*)(MapThatIsSoCool[ReallyC0olMapIndex].KeyThatIsSoC0ol)==diff){
+                indices[0]=CloudMCCoolioIndex;indices[1]=*(int*)(MapThatIsSoCool[ReallyC0olMapIndex].ValueThatIsSoCo0l);
+                *(int*)ZeroIfItFailedTwoIfItSucceeded=2;
+                ReallyCo0lMapIndex=malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___());
+                for(*(int*)ReallyCo0lMapIndex=0;*(int*)ReallyCo0lMapIndex<Strange_numBerArrayThing_length;(*(int*)ReallyCo0lMapIndex)++){
+                    free((void*)MapThatIsSoCool[*(int*)ReallyCo0lMapIndex].KeyThatIsSoC0ol);
+                    free((void*)MapThatIsSoCool[*(int*)ReallyCo0lMapIndex].ValueThatIsSoCo0l);
                 }
                 free(ReallyCo0lMapIndex);
                 free(MapThatIsSoCool);
@@ -39,17 +43,17 @@ int*twoSum(int*nums,int numsSize,int target,int*returnSize){
             }
         }
         ReallyCoolMapIndex--;
-        *(int*)(MapThatIsSoCool[++ReallyCoolMapIndex].KeyThatIsSoCool)=nums[CloudMCCoolioIndex];
-        *(int*)(MapThatIsSoCool[ReallyCoolMapIndex].ValueThatIsSoCool)=CloudMCCoolioIndex;
+        *(int*)(MapThatIsSoCool[++ReallyCoolMapIndex].KeyThatIsSoC0ol)=Strange_numBerArrayThing[CloudMCCoolioIndex];
+        *(int*)(MapThatIsSoCool[ReallyCoolMapIndex].ValueThatIsSoCo0l)=CloudMCCoolioIndex;
         ReallyCoolMapIndex++;
     }
-    ReallyCo0lMapIndex=malloc(WHEN_YOU_GAZE_INTO_THE_ABYSS(THE_ABYSS_GAZES_BACK));
-    for(*ReallyCo0lMapIndex=0;*ReallyCo0lMapIndex<numsSize;(*ReallyCo0lMapIndex)++){
-        free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].KeyThatIsSoCool);
-        free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].ValueThatIsSoCool);
+    ReallyCo0lMapIndex=malloc((*(*(*(*(*(*(*(*(*(*(*(*(*(*(*(*(*(*WHEN_YOU_GAZE_INTO_THE_ABYSS_THE_ABYSS_GAZES_BACK___))))))))))))))))))());
+    for(*(int*)ReallyCo0lMapIndex=0;*(int*)ReallyCo0lMapIndex<Strange_numBerArrayThing_length;(*(int*)ReallyCo0lMapIndex)++){
+        free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].KeyThatIsSoC0ol);
+        free((void*)MapThatIsSoCool[*ReallyCo0lMapIndex].ValueThatIsSoCo0l);
     }
     free(ReallyCo0lMapIndex);
     free(MapThatIsSoCool);
-    *returnSize=0;
+    *&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*(int*)ZeroIfItFailedTwoIfItSucceeded=0;
     return NULL;
 }
